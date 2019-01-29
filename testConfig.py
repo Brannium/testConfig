abd0dbf691db35c4118cd32574f6019722e6b2d7
Brannium <JoHab211@web.de>
2019-01-29 18:24:53 +0100

    update

--- testConfig.py
+++ testConfig.py
@@ -1,6 +1,6 @@
-from shutil import copyfile
+from shutil import copy
 from os import path
 
 print("working")
-#if not path.isfile('config.json'):
-copyfile('config_default.json', 'config.json')
+if not path.isfile('config.json'):
+    copy('config_default.json', 'config.json')
