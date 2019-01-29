f2853e9e6eeb64498c76c2d4e8f8c1e9c5c85d84
Brannium <JoHab211@web.de>
2019-01-29 18:47:19 +0100

    update

--- testConfig.py
+++ testConfig.py
@@ -3,7 +3,9 @@ from shutil import copy2
 from os import path
 
 print("working")
-print(os.getcwd())
+print('os.getcwd(): %s' % os.getcwd())
+print('os.path-dirname(os.path.realpath(__file__)): %s' % os.path.dirname(os.path.realpath(__file__)))
+print('os.path.dirname(os.path.abspath(__file__)): %s' % os.path.dirname(os.path.abspath(__file__)))
 
 if not path.isfile('config.json'):
     copy2('config_default.json', 'config.json')
