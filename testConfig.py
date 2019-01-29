51f47b7e62f1b7cdea804f43c9efe4f6a2cd38f8
Brannium <JoHab211@web.de>
2019-01-29 18:52:15 +0100

    update

--- testConfig.py
+++ testConfig.py
@@ -8,3 +8,5 @@ print('os.listdir(os.getcwd()): %s' % os.listdir(os.getcwd()))
 
 if not path.isfile('config.json'):
     copy2('config_default.json', 'config.json')
+
+print('os.listdir(os.getcwd()): %s' % os.listdir(os.getcwd()))
